3dffd6ac553313881530b5d7b86d85c3313a37ae
danieltpenny <danieltpenny@gmail.com>
2020-01-24 23:22:09 -0400

    Assignment2 - SOAP web service

---
+++ Assignment2/src/com/dpenny/mcda5550/Assignment2.java
@@ -0,0 +1,53 @@
+package com.dpenny.mcda5550;
+
+import java.sql.Connection;
+import java.util.Collection;
+
+import com.dpenny.mcda5550.connect.ConnectionFactory;
+import com.dpenny.mcda5550.connect.MySQLJDBCConnection;
+import com.dpenny.mcda5550.dao.MySQLAccess;
+import com.dpenny.mcda5550.entity.Transaction;
+
+public class Assignment2 {
+
+	public static Connection single_instance;
+
+	public static Connection getInstance() {
+		if (single_instance == null) {
+			MySQLJDBCConnection dbConnection = new MySQLJDBCConnection();
+			single_instance = dbConnection.setupConnection();
+		}
+
+		return single_instance;
+	}
+
+	public static void main(String[] args) {
+		MySQLAccess dao = new MySQLAccess();
+		try {
+			 //Connection connection = getInstance();
+
+			ConnectionFactory factory = new ConnectionFactory();
+			Connection connection = factory.getConnection("mySQLJDBC");
+
+			//Connection connection = factory.getConnection("fake");
+			
+			Collection<Transaction> trxns = dao.getAllTransactions(connection);
+
+			for (Transaction trxn : trxns) {
+				trxn.setNameOnCard("New Value");
+				dao.updateTransaction(connection, trxn);
+				
+				System.out.println(trxn.toString());
+			}
+
+			if (connection != null) {
+				connection.close();
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Assignment2/src/com/dpenny/mcda5550/connect/DBConnection.java
@@ -0,0 +1,8 @@
+package com.dpenny.mcda5550.connect;
+import java.sql.Connection;
+
+public interface DBConnection {
+
+	public Connection setupConnection();
+	
+}
